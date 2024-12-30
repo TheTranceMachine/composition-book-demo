6e02f965da17d9ab69ecd0bb24e8c3fd566e2fa2
Greg Smolin <grzegorz.smolin2@gmail.com>
2024-12-30 18:02:40 -0500

    feature - landing page - fix link

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -21,7 +21,7 @@ const Hero = () => {
                 </p>
                 <div className="flex flex-col items-center justify-center space-y-4 sm:flex-row sm:space-x-4 sm:space-y-0">
                   <Link
-                    href="https://nextjstemplates.com/templates/saas-starter-startup"
+                    href="/workspace"
                     className="rounded-sm bg-sandwisp px-8 py-4 text-base font-semibold text-black duration-300 ease-in-out hover:bg-sandwisp/80"
                   >
                     🔥 Demo
