65d434e65ea1fa5ff326185784901f99f370b688
Greg Smolin <grzegorz.smolin2@gmail.com>
2025-01-26 13:34:39 -0500

    bugfix - context menu - mobile - prevent text selection

--- src/app/workspace/_components/fileExplorer/components/NoFilesSpace.tsx
+++ src/app/workspace/_components/fileExplorer/components/NoFilesSpace.tsx
@@ -7,7 +7,7 @@ type NoFilesSpaceTypes = {
 };
 
 const NoFilesSpace = ({ panelExpanded, setNewFile, panelElement }: NoFilesSpaceTypes) => (
-  <div className="h-32 p-3">
+  <div className="no-file-space h-32 p-3">
     <CustomContextMenu handleInput={(val) => setNewFile({ directoryId: "", ...val })} panelElement={panelElement}>
       <div className="flex items-center justify-center text-white border-2 border-dashed border-white p-3 h-full">
         {panelExpanded ? "No files found. Right click here to create a new file or folder" : "Right click"}
